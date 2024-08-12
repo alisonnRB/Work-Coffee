250db40dacf8ee76d19e1aac12c476561e744a48
alisonnRB <alisonrbjs@gmail.com>
2024-08-12 16:15:27 -0300

    responsive

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import Room from '../../assets/aboutImage.png';
 import Form from '../../assets/aboutForm.svg';
 
+import './index.css';
+
 export default function About() {
     return (
         <section className="flex w-full bg-pink-950 relative top-96 z-[300] overflow-x-clip">
@@ -25,15 +27,15 @@ export default function About() {
                 />
 
                 <span className="flex flex-col justify-around items-center z-20 h-full w-[70%] mt-[6%] mr-[13%]">
-                    <h1 className="text-white font-bold relative text-[4rem]">
+                    <h1 className="text-white font-bold relative text-[4rem] aboutTitle">
                         SOBRE A WORK COFFEE
                     </h1>
 
-                    <p className="text-white text-center relative font-medium text-[1.8rem] w-[80%]">
+                    <p className="text-white text-center relative font-medium  w-[80%] aboutText">
                         Na WorkCoffe, acreditamos na criação do ambiente perfeito para você ler, estudar ou trabalhar em paz. Nosso café oferece um ambiente sereno e tranquilo, projetado para ajudar você a se concentrar e relaxar. Esteja você aqui para saborear nosso café cuidadosamente preparado, desfrutar de uma bebida quente ou saborear uma de nossas deliciosas refeições, estamos empenhados em fornecer a você um espaço confortável e produtivo.
                     </p>
                     <Link href={'/'} className="w-full ml-[25%]">
-                        <button className="border-solid border-white text-white text-[2rem] font-medium border-[.2rem] py-[1rem] w-[75%]">
+                        <button className="border-solid border-white text-white aboutText font-medium border-[.2em] py-[1em] w-[75%]">
                             LER MAIS
                         </button>
                     </Link>
