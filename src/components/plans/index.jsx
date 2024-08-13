627df6eac5024a8470c5ca4ec0a93f0c72f3cc78
alisonnRB <alisonrbjs@gmail.com>
2024-08-13 20:40:07 -0300

    created plans page

--- src/components/plans/index.jsx
+++ src/components/plans/index.jsx
@@ -3,7 +3,7 @@ import plans from '../../assets/plans.png';
 
 export default function Plans() {
     return (
-        <section className='relative flex flex-col items-center justify-between z-[250]'>
+        <section className='relative flex flex-col items-center justify-between z-[250] pb-[20%] bg-white'>
             <Image
                 src={plans}
                 objectFit='contain'
@@ -18,7 +18,7 @@ export default function Plans() {
 
             <div className='w-full flex flex-col items-center z-20 text-white font-semibold gap-6'>
                 <p className='text-center text-[3vw] w-[75%]'>O workcoffee oferece um ambiente ideial para trabalhar, estudar e relaxar. Confira também os planos para acesso ao nosso café</p>
-                <button className='bg-red text-[2vw] px-4 py-1'>VER PLANOS</button>
+                <button className='bg-red text-[2vw] px-[2em] py-1'>VER PLANOS</button>
             </div>
 
         </section>
