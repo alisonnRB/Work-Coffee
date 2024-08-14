6d78c653ecb230563e19ff3dc1b9b48788d9951f
alisonnRB <alisonrbjs@gmail.com>
2024-08-14 14:21:59 -0300

    plans section are responsive

--- src/components/plans/index.jsx
+++ src/components/plans/index.jsx
@@ -1,9 +1,11 @@
 import Image from 'next/image';
 import plans from '../../assets/plans.png';
 
+import './index.css';
+
 export default function Plans() {
     return (
-        <section className='relative flex flex-col items-center justify-between z-[250] pb-[20%] bg-white'>
+        <section className='relative flex flex-col items-center gaper z-[250] pb-[20%] bg-white'>
             <Image
                 src={plans}
                 objectFit='contain'
@@ -11,14 +13,14 @@ export default function Plans() {
                 quality={100}
             />
 
-            <span className='z-20 p-4 w-full mb-[15%]'>
-                <h1 className='text-white font-semibold text-[5vw]'>TRABALHE COM UM <br /> BOM CAFÉ</h1>
-                <p className='font-semibold text-[2vw]'>R$ 5,00 /h</p>
+            <span className='z-20 p-4 w-full'>
+                <h1 className='text-white font-semibold plans-title'>TRABALHE COM UM <br /> BOM CAFÉ</h1>
+                <p className='font-semibold plans-price'>R$ 5,00 /h</p>
             </span>
 
             <div className='w-full flex flex-col items-center z-20 text-white font-semibold gap-6'>
-                <p className='text-center text-[3vw] w-[75%]'>O workcoffee oferece um ambiente ideial para trabalhar, estudar e relaxar. Confira também os planos para acesso ao nosso café</p>
-                <button className='bg-red text-[2vw] px-[2em] py-1'>VER PLANOS</button>
+                <p className='text-center plans-text w-[75%]'>O workcoffee oferece um ambiente ideial para trabalhar, estudar e relaxar. Confira também os planos para acesso ao nosso café</p>
+                <button className='bg-red plans-text px-[2em] py-1'>VER PLANOS</button>
             </div>
 
         </section>
